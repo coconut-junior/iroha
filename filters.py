4239fad18c7a98ac202cd11d31d66aa60a168d89
Jimmy Blanck <blanckjm@gmail.com>
2021-03-06 13:24:38 -0500

    os detection implemented

--- filters.py
+++ filters.py
@@ -2,9 +2,18 @@ import os
 import sys
 
 home_dir = os.getcwd()
-factor = 0.7
+binary_dir = ''
+
+if os.name == 'nt':
+    binary_dir = '/bin/win32/'
+elif os.name == 'darwin':
+    binary_dir = '/bin/darwin/'
+else:
+    binary_dir = ''
+
+factor = 0.75
 tempo = 1
 
 def pitch():
     print(home_dir)
-    os.system(home_dir + '/bin/ffmpeg -y -i speech.mp3 -af "asetrate=44100*' + str(factor) + ',atempo=' + str(tempo) + ',aresample=44100" output.mp3')
+    os.system(home_dir + binary_dir + 'ffmpeg -y -i speech.mp3 -af "asetrate=44100*' + str(factor) + ',atempo=' + str(tempo) + ',aresample=44100" output.mp3')
